ec1bf25bd8fc239407adeca0072027ef4bce2c6c
NahlaP <149507268+NahlaP@users.noreply.github.com>
2023-11-05 13:58:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// function validate(){
+//     var Name=document.getElementById("name");
+//     var email=document.getElementById("email");
+//     var phoneno=document.getElementById("phone");
+//     var message=document.getElementById("iq");
+    
+//     if(Name.value==""){
+//         alert("must have a name");
+//         document.contactform.name.focus();
+//         return false;
+//     }
+
+//     if(email.value==""){
+//         alert("must have a email");
+//         document.contactform.email.focus();
+//         return false;
+//     }
+
+
+//     if(phoneno.value==""){
+//         alert("must have a phoneno");
+//         document.contactform.phone.focus();
+//         return false;
+//     }
+
+//     if(message.value==""){
+//         alert("must have a message");
+//         document.contactform.iq.focus();
+//         return false;
+//     }
+//     return true;
+// }
